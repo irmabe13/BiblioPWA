079db443e52bbb6c982493c609473453bef98694
Irma BEN AHAMADI <irma79200@gmail.com>
2024-01-31 16:12:05 +0100

    adding a sw + handling install, activate and fetch events

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
 console.log('Hello depuis le service worker');
+
+self.addEventListener('install', (evt) => {
+    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log(`sw activé à ${new Date().toLocaleTimeString()}`);    
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('fetch sur url', evt.request.url);
+});
