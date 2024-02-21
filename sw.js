26f18577e755c8cfe8603dc2b6bee5a9bff66ce6
Irma BEN AHAMADI <irma79200@gmail.com>
2024-02-21 16:21:59 +0100

    adding a network first with cache fallback caching strategy

--- sw.js
+++ sw.js
@@ -12,76 +12,68 @@ self.addEventListener('fetch', (evt) => {
     console.log('fetch sur url', evt.request.url);
 });
 
-self.addEventListener('fetch', (evt) => {
-if(!navigator.onLine) {
-    const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
-    evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Apllication en mode dégradé. Veuillez vous connecter</div>', headers));
-}
-});
-
 const cacheName = 'biblio' + '1.1';
 
-self.addEventListener('install', (evt) => {
+self.addEventListener('install', evt => {
+  console.log('install evt', evt);
+  caches.open(cacheName).then(cache => {
+    cache.addAll([
+    'index.html',
+    'main.js',
+    'style.css',
+    'add_book.html',
+    'add_book.js',
+    ])
+  })
+});
 
-    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
-
-    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);       
-
-    const cachePromise = caches.open(cacheName).then(cache => {
-
-        return cache.addAll([
-
-            'index.html',
-
-            'main.js',
-
-            'style.css',
-
-            'add_book.html',
-
-            'add_book.js',
-            
-            'contact.html',
-            
-            'contact.js'
-        ])
-
-        .then(console.log('cache initialisé'))
-
-        .catch(console.err);
-
-    });
-
-    self.addEventListener('activate', evt => {
-        console.log('activate evt', evt);
-      });
-
-
-    evt.waitUntil(cachePromise);
-
-
+self.addEventListener('activate', evt => {
+  console.log('activate evt', evt);
 });
 
 self.addEventListener('fetch', evt => {
-    if(!navigator.onLine) {
-      const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'}};
-      evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Application en mode dégradé. Veuillez vous connecter</div>', headers));
-    }
-    console.log('fetch event sur url ',evt.request.url);
-  
-    // stratégie de cache only with network fallback
+  if(!navigator.onLine) {
+    const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'}};
+    evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Application en mode dégradé. Veuillez vous connecter</div>', headers));
+  }
+  console.log('fetch event sur url ',evt.request.url);
+
+  self.addEventListener('fetch', evt => {
+
     evt.respondWith(
-      caches.match(evt.request).then (res => {
-          if(res) {
-             
-              return res;
-            }
-            return fetch(evt.request). then(newResponse => {
-                   
-              caches.open(cacheName). then (cache => cache.put(evt.request, newResponse));
-        
-              return newResponse.clone();
-            })
-          })
-      );
-  });
+
+        fetch(evt.request).then( res => {
+
+            if (evt.request.url.includes('add_book') ) {
+
+
+                throw Error('inaccessible');
+
+              
+
+              }
+
+              console.log('${evt.request.url} fetchée depuis le réseau');
+
+            // we add the latest version into the cache
+
+            caches.open(cacheName).then(cache => cache.put(evt.request, res));
+
+            // we clone it as a response can be read only once (it's like a one time read stream)
+
+            return res.clone();
+
+        })
+
+        .catch(err => {
+
+            console.log('${evt.request.url} fetchée depuis le cache');
+
+            caches.match(evt.request);
+
+        })
+
+    );
+
+});
+});
